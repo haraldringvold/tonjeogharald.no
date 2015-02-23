bcba6df5aa462a6a5e463430fbb75d1fe7196a4b
Harald Ringvold <harald.ringvold@gmail.com>
2015-02-23 22:47:36 +0100

    simple version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+  var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'tonjeogharald',
+        clientId: 'f9bb415a53324270abe05ca10713aeeb',
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        resolution: 'low_resolution'
+    });
+    feed.run();
+
+});
