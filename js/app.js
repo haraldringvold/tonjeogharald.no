a445a7f16998cc08e01024acfb346254858b23f3
Harald Ringvold <harald.ringvold@gmail.com>
2015-10-11 23:11:15 +0200

    Fix grid problems

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ $(function() {
         get: 'tagged',
         tagName: 'tonjeogharald',
         clientId: 'f9bb415a53324270abe05ca10713aeeb',
-        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
-        resolution: 'low_resolution'
+        template: '<a href="{{link}}" class="small-12 medium-6 large-4 columns"><img src="{{image}}" /></a>',
+        resolution: 'standard_resolution'
     });
     feed.run();
 
